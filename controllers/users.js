ab965588e85be75d73be76752344a0625cb4e0e6
Aleksandra Mishina <valtsevaaleksandra@gmail.com>
2023-07-11 14:50:32 +0300

    fifth try

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ const User = require('../models/user');
 module.exports.getUser = (req, res) => {
   User.find({})
     .then((users) => res.send(users))
-    .catch((err) => res.status(500).send({ message: err.message }));
+    .catch(() => res.status(500).send({ message: 'Ошибка 500' }));
 };
 
 module.exports.createUser = (req, res) => {
@@ -12,9 +12,8 @@ module.exports.createUser = (req, res) => {
     .then((user) => res.status(201).send(user))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(400)
-          .send({ message: 'Невозможно создать пользователя' });
-      } else { res.status(500).send({ message: err.message }); }
+        res.status(400).send({ message: 'Невозможно создать пользователя' });
+      } else { res.status(500).send({ message: 'Ошибка 500' }); }
     });
 };
 
@@ -22,37 +21,38 @@ module.exports.getUserId = (req, res) => {
   const { userId } = req.params;
   User.findById(userId)
     .orFail(new Error('NotFound'))
-    .then((user) => res.send(user))
+    .then((user) => res.status(201).send(user))
     .catch((err) => {
       if (err.name === 'CastError') {
-        return res.status(400)
-          .send({ message: 'Данные не верны' });
+        return res.status(400).send({ message: 'Данные не верны' });
       } if (err.message === 'NotFound') {
         return res.status(404).send({ message: 'Пользователь не найден' });
       }
-      return res.status(500).send({ message: err.message });
+      return res.status(500).send({ message: 'Ошибка 500' });
     });
 };
 
 module.exports.changeUserInfo = (req, res) => {
   const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
+  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true })
+    .orFail()
     .then((user) => res.status(200).send(user))
     .catch((err) => {
       if (err.name === 'ValidationError' || err.name === 'CastError') {
         return res.status(400).send({ message: 'Невозможно обновить профиль' });
       }
-      return res.status(500).send({ message: err.message });
+      return res.status(500).send({ message: 'Ошибка 500' });
     });
 };
 
 module.exports.changeAvatar = (req, res) => {
   const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
+  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
+    .orFail()
     .then((user) => res.status(200).send(user))
     .catch((err) => {
       if (err.name === 'ValidationError' || err.name === 'CastError') {
         res.status(400).send({ message: 'Данные переданы некорректно' });
-      } else { res.status(500).send({ message: err.message }); }
+      } else { res.status(500).send({ message: 'Ошибка 500' }); }
     });
 };
