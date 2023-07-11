b1cc257c9b7d3c2678661ea3fa6dfd06f9f32e8d
Aleksandra Mishina <valtsevaaleksandra@gmail.com>
2023-07-11 15:02:20 +0300

    11

--- controllers/users.js
+++ controllers/users.js
@@ -13,7 +13,7 @@ module.exports.createUser = (req, res) => {
     .catch((err) => {
       if (err.name === 'ValidationError') {
         res.status(400).send({ message: 'Невозможно создать пользователя' });
-      } else { res.status(500).send({ message: 'Ошибка 500' }); }
+      } else { res.status(500).send({ message: err.message }); }
     });
 };
 
@@ -21,14 +21,14 @@ module.exports.getUserId = (req, res) => {
   const { userId } = req.params;
   User.findById(userId)
     .orFail(new Error('NotFound'))
-    .then((user) => res.status(201).send(user))
+    .then((user) => res.status(200).send(user))
     .catch((err) => {
       if (err.name === 'CastError') {
         return res.status(400).send({ message: 'Данные не верны' });
       } if (err.message === 'NotFound') {
         return res.status(404).send({ message: 'Пользователь не найден' });
       }
-      return res.status(500).send({ message: 'Ошибка 500' });
+      return res.status(500).send({ message: err.message });
     });
 };
 
@@ -41,18 +41,17 @@ module.exports.changeUserInfo = (req, res) => {
       if (err.name === 'ValidationError' || err.name === 'CastError') {
         return res.status(400).send({ message: 'Невозможно обновить профиль' });
       }
-      return res.status(500).send({ message: 'Ошибка 500' });
+      return res.status(500).send({ message: err.message });
     });
 };
 
 module.exports.changeAvatar = (req, res) => {
   const { avatar } = req.body;
   User.findByIdAndUpdate(req.user._id, { avatar }, { new: true })
-    .orFail()
     .then((user) => res.status(200).send(user))
     .catch((err) => {
       if (err.name === 'ValidationError' || err.name === 'CastError') {
         res.status(400).send({ message: 'Данные переданы некорректно' });
-      } else { res.status(500).send({ message: 'Ошибка 500' }); }
+      } else { res.status(500).send({ message: err.message }); }
     });
 };
