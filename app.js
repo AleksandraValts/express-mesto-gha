1fc7076d15743c0436dffee3fdc1fb5027b91507
Aleksandra Mishina <valtsevaaleksandra@gmail.com>
2023-07-12 14:33:28 +0300

    helmet added

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 const router = require('./routes/router');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
