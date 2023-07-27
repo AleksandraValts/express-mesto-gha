fbe8e47e223dd710ee3ebcce55bec5be9f28a8f7
Aleksandra Mishina <valtsevaaleksandra@gmail.com>
2023-07-27 12:31:48 +0300

    delete

--- controllers/cards.js
+++ controllers/cards.js
@@ -27,16 +27,24 @@ module.exports.createCard = (req, res, next) => {
 };
 
 module.exports.deleteCard = (req, res, next) => {
-  const { cardId } = req.params;
-  return Card.findById(cardId)
+  const { id: cardId } = req.params;
+  const { userId } = req.user;
+  Card.findById({ _id: cardId })
     .then((card) => {
       if (!card) {
         throw new NotFound('Карточка не найдена');
       }
-      if (!card.owner.equals(req.user._id)) {
-        return next(new Forbidden('Ошибка прав доступа'));
+      const { owner: cardOwnerId } = card;
+      if (cardOwnerId.valueOf() !== userId) {
+        throw new Forbidden('Ошибка доступа');
       }
-      return card.remove().then(() => res.send({ message: 'Карточка удалена' }));
+      return Card.findByIdAndDelete(cardId);
+    })
+    .then((deletedCard) => {
+      if (!deletedCard) {
+        throw new NotFound('Карточка не найдена');
+      }
+      res.send({ data: deletedCard });
     })
     .catch(next);
 };
