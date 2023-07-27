ae5a6be3bbb8eae39fafa1094fb1859068f2c5c5
Aleksandra Mishina <valtsevaaleksandra@gmail.com>
2023-07-27 12:23:27 +0300

    id

--- models/card.js
+++ models/card.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const Card = new mongoose.Schema({
   name: {
@@ -9,7 +10,11 @@ const Card = new mongoose.Schema({
   },
   link: {
     type: String,
-    required: [true, 'Поле обязательно к заполнению'],
+    required: true,
+    validate: {
+      validator: (url) => validator.isURL(url),
+      message: 'Неправильный формат URL',
+    },
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
